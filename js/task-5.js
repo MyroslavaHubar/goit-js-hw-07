640a95c284ef20d45e48fb931f0f4d7fa2209319
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-05-21 23:24:36 +0200

    add js file task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnElem = document.querySelector('.change-color');
+// console.log(btnElem);
+const btnSpanElem = document.querySelector('.color');
+// console.log(btnSpanElem);
+
+btnElem.addEventListener('click', () => {
+  const bodyColor = getRandomHexColor();
+  document.body.style.backgroundColor = bodyColor;
+  btnSpanElem.textContent = bodyColor;
+});
