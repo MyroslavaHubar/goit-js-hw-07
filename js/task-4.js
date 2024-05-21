9ef7ab26f0caa6ff2f1b6e4f5b8f3501ada0e9c6
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-05-21 03:11:22 +0200

    update task 4 js file

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const formElem = document.querySelector('.login-form');
+// console.log(formElem);
+formElem.addEventListener('submit', event => {
+  event.preventDefault();
 
+  const data = {
+    email: formElem.elements.email.value.trim(),
+    password: formElem.elements.password.value.trim(),
+  };
+
+  if (email === '' || password === '') {
+    alert('All form fields must be filled in');
+  } else {
+    console.log(data);
+    formElem.reset();
+  }
+});
